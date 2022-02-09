a2ecdc014bae50ffccefc3f87d1c6bf2960734a7
shakarr <risaaflo@gmail.com>
2022-02-09 19:00:15 +0100

    add component: Controls

---
+++ src/components/Coffie/Coffie.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Coffie.scss";
+
+export default function Coffie() {
+  return (
+    <div className="container-coffie">
+      <div className="container-coffie__content">
+        <a id="coffie" href="https://ko-fi.com/W7W27UVLB" target="_blank" rel="noreferrer">
+          <img
+            height="36"
+            src="https://cdn.ko-fi.com/cdn/kofi2.png?v=3"
+            border="0"
+            alt="Buy Me a Coffee at ko-fi.com"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
