6dfc6fcdfa5b7aa4f4e5826a01081aecad37f996
shakarr <risaaflo@gmail.com>
2022-02-09 19:03:28 +0100

    add component: Controls

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Banner.scss";
+import logo from "../../assets/img/logo/Shakar.png"
+export default function Banner() {
+  return (
+      <div className='container-banner'>
+          <div className='container-banner__content'>
+              <a href=''>
+              <img src={logo} alt='banner'/>
+              </a>
+              
+          </div>
+      </div>
+  );
+}
